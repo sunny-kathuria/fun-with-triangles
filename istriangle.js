64f84706ba4ce19c4a27d08f777c61730591aefa
Sunny Kathuria <51099.sk@gmail.com>
2021-08-25 22:15:42 +0530

    quiz section done

--- istriangle.js
+++ istriangle.js
@@ -10,10 +10,10 @@ checkButton.addEventListener("click", checkTriangle)
 function checkTriangle(){
     
     const sum=Number(angle1.value)+Number(angle2.value)+Number(angle3.value);
-    if(sum===180){ outputBox.value="Hurrahhh!!This is a Traingle";
+    if(sum===180){ outputBox.value="Hurrahhh!!This forms a Traingle";
 
     }
     else{
-        outputBox.value="Try, some other values";
+         outputBox.value="Try, some other values";
     }
 }
