b217928c17e2cee62e6c6e25ec51721d87665cf4
Sunny Kathuria <51099.sk@gmail.com>
2021-09-09 20:33:22 +0530

    area completed

--- area.js
+++ area.js
@@ -5,6 +5,9 @@ const calculateAreaBtn=document.querySelector("#calculateArea-btn");
 
 function calculateAndShow(){
     console.log(base.value);
+    var area=0;
+    area=(base.value*height.value)/2;
+    output.innerHTML="Area of triangle is "+area;
 }
 
 calculateAreaBtn.addEventListener("click", calculateAndShow);
